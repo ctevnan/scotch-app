59fdeb1d32fbb9f55199893bdced1fb6221e65ec
ctevnan <carolyntevnan@yahoo.com>
2016-09-29 17:40:39 -0400

    adding angular forms, http, router and upgrade

---
+++ app/magic.js
@@ -0,0 +1,7 @@
+var message = 'hello';
+function saySomething(message) {
+    if (message === void 0) { message = 'whats up'; }
+    console.log(message);
+}
+saySomething(message);
+//# sourceMappingURL=magic.js.map
