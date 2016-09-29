f358454fb1775002a6d4243f9756ec66b5662591
ctevnan <carolyntevnan@yahoo.com>
2016-09-29 19:21:13 -0400

    adding navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">My Angular 2 App</a>
+        </div>
+      </nav>
+    </header>
+
     <div class="jumbotron">
       <h1>Welcome to our app</h1>
     </div>
+
+    footer
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
